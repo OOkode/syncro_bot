f7eb46a22e4d7719029ff04d9c8c0e94d2f0b461
alereyesx <63877846+alereyesx@users.noreply.github.com>
2020-07-13 01:03:36 -0400

    Starting to be able to select groups

--- app.py
+++ app.py
@@ -3,9 +3,18 @@ import env
 
 PERMITTED_USERS = ['ChelpBots']
 
-channel = -1001360204322
-group = 'tuenvio_granma'
-log_channel = -1001394912157
+channels = {'Granma':-1001360204322,'Pinar': 'AlertaTuenvioPinar'}
+groups = {'Granma': 'tuenviogranma','Pinar':'TuenvioPinardelRio'}
+log_channels = {'Granma':-1001394912157,'Pinar':'logpinardelrio'}
+
+
+channel = channels['Pinar']
+group = groups['Pinar']
+log_channel = log_channels['Pinar']
+
+def get_api_credentials():
+    return (env.API_ID,env.API_HASH)
+
 
 def get_users_not_in_group(client,channel,group):
 
@@ -35,31 +44,59 @@ def get_users_without_username(client,group):
     
     return UNWANTED_USERS
 
-def purge_unwanted_users(client,channel,group,log_channel):
+def get_users_in_forbidden_groups(client,forbidden_groups):
+
+    UNWANTED_USERS = []
+
+    for group in forbidden_groups:
+        for user in client.get_participants(group):
+            UNWANTED_USERS.append(user)
+
+    return UNWANTED_USERS            
+
+def purge_unwanted_users(client,channel,group,log_channel,forbidden_groups):
+
+    group_users = client.get_participants(group)
 
     try:
 
         users_to_be_kicked_from_channel = get_users_not_in_group(client,channel,group)
         users_to_be_kicked_from_group = get_users_without_username(client,group)
+        users_in_forbidden_groups = get_users_in_forbidden_groups(client,forbidden_groups)
 
         for user in users_to_be_kicked_from_group:
             client.kick_participant(entity=group,user=user)
-            client.send_message(entity=log_channel,message= f'Kicked {user.id if not user.username else user.username} for not having username')
+            username = ('@' + user.username) if user.username else ''
+            client.send_message(entity=log_channel,message= f'Kicked {user.id if not user.username else username} for not having username')
 
         for user in users_to_be_kicked_from_channel:
             client.kick_participant(entity=group,user=user)
-            client.send_message(entity=log_channel,message= f'Kicked {user.id if not user.username else user.username} for not subscribing to the group')
+            username = ('@' + user.username) if user.username else ''
+            client.send_message(entity=log_channel,message= f'Kicked {user.id if not user.username else username} for not subscribing to the group')
+
+        # for user in users_to_be_kicked_from_channel:
+        #     client.edit_permissions(channel, user, view_messages=False)
+        #     username = ('@' + user.username) if user.username else ''
+        #     client.send_message(entity=log_channel,message= f'Banned {user.id if not user.username else username} for not subscribing to the group')
+
+
+        for user in users_in_forbidden_groups:
+            if user in group_users:
+                client.edit_permissions(channel, user, view_messages=False)
+                client.edit_permissions(group, user, view_messages=False)
+                username = ('@' + user.username) if user.username else ''
+                client.send_message(entity=log_channel,message= f'Banned {user.id if not user.username else username} for belonging to forbidden group')
             
 
     except ValueError as e:
         raise e
 
-api_id = env.API_ID
-api_hash = env.API_HASH
+
+api_id, api_hash = get_api_credentials()
 
 client = TelegramClient('session1', api_id, api_hash)
 client.start(phone=env.PHONE,password=env.PASSWORD)
 
-purge_unwanted_users(client,channel,group,log_channel)
+purge_unwanted_users(client,channel,group,log_channel,['tuenviopinargrupo'])
 
 
