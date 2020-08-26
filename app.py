a9160083d9b6673c0663b721576de49f2db78eca
alereyesx <63877846+alereyesx@users.noreply.github.com>
2020-08-26 12:20:22 -0400

    Improved something?

--- app.py
+++ app.py
@@ -2,12 +2,20 @@ from telethon import TelegramClient, events, sync
 from telethon.errors.rpcerrorlist import UserAdminInvalidError
 import env
 import time, asyncio
+from telethon.sync import TelegramClient
+from telethon import functions, types
 
 ALLOWED_USERS = ['ChelpBots']
 
-channels = {'Granma': -1001360204322, 'Pinar': 'AlertaTuenvioPinar'}
-groups = {'Granma': 'tuenviogranma', 'Pinar': 'TuenvioPinardelRio'}
-log_channels = {'Granma': -1001394912157, 'Pinar': 'logpinardelrio'}
+channels = {'Granma': -1001360204322, 'Pinar': 'AlertaTuenvioPinar','Artemisa':'AlertaTuenvio_Artemisa',
+            'Santa_Clara': 'TuEnvio_Canal'}
+
+groups = {'Granma': 'tuenviogranma', 'Pinar': 'TuenvioPinardelRio','Artemisa':'TuenvioArtemisa',
+          'Santa_Clara': 'TuEnvio_Sta_Clara'}
+
+log_channels = {'Granma': -1001394912157, 'Pinar': 'logpinardelrio','Artemisa':'logsartemisa',
+                'Santa_Clara': -1001219375136}
+
 forbidden_groups = ['tuenviopinargrupo', ]
 
 CHANNEL = channels['Granma']
@@ -27,11 +35,9 @@ async def get_users_not_in_group(client, channel_users, group_users):
     global ALLOWED_USERS
     unwanted_users = []
 
-
-
-
     for user in channel_users:
-        if ((user.username not in ALLOWED_USERS) and (not user.bot) and (not (user in group_users)) or ( not user.username)):
+        if ((user.username not in ALLOWED_USERS) and (not user.bot) and (not (user in group_users)) or (
+        not user.username)):
             print(user.username or user.id)
             unwanted_users.append(user)
 
@@ -84,8 +90,6 @@ async def kick_from_channel(client, users_to_be_kicked_from_channel):
             except UserAdminInvalidError as e:
                 print(f"No se pude eliminar al usuario {user.username if user.username else user.id}")
 
-
-
             action_counter += 2
 
 
@@ -106,8 +110,6 @@ async def ban_from_channel(client, users_to_be_banned_from_channel):
                                           message=f'Banned {user.id if not user.username else username} for not subscribing to the group')
             except UserAdminInvalidError as e:
                 print(f"No se pude eliminar al usuario {user.username if user.username else user.id}")
-
-
 
             action_counter += 2
 
@@ -137,20 +139,26 @@ async def forbidden_group_ban(client, users_in_forbidden_groups):
             time.sleep(100)
 
         if not user.bot:
-            await client.edit_permissions(CHANNEL, user.id, view_messages=False)
-            await client.edit_permissions(GROUP, user.id, view_messages=False)
-            time.sleep(5)
-            username = ('@' + user.username) if user.username else ''
-            await client.send_message(entity=LOG_CHANNEL,
-                                      message=f'Banned {user.id if not user.username else username} for belonging to forbidden group')
+            try:
+                await client.edit_permissions(CHANNEL, user.id, view_messages=False)
+                await client.edit_permissions(GROUP, user.id, view_messages=False)
+                time.sleep(5)
+                username = ('@' + user.username) if user.username else ''
+                await client.send_message(entity=LOG_CHANNEL,
+                                          message=f'Banned {user.id if not user.username else username} for belonging to forbidden group')
+
+            except UserAdminInvalidError as e:
+                print(f"No se pude eliminar al usuario {user.username if user.username else user.id}")
+
             action_counter += 2
 
 
 async def purge_unwanted_users(client, channel, group, log_channel, forbidden_groups):
-    group_users = await client.get_participants(entity=group, aggressive=True)
     channel_users = await client.get_participants(entity=channel, aggressive=True)
-    print(len(group_users))
-    print(len(channel_users))
+    group_users = await client.get_participants(entity=group, aggressive=True,
+                                                filter=types.ChannelParticipantsSearch(''), limit=10000)
+    print(f"Usuarios en grupo {len(group_users)}")
+    print(f"Usuarios en canal {len(channel_users)}")
 
     try:
 
@@ -168,15 +176,12 @@ async def purge_unwanted_users(client, channel, group, log_channel, forbidden_gr
             #
             # await kick_from_channel(client, not_abyding_users_from_channel)
             #
-            await ban_from_channel(client,not_abyding_users_from_channel)
+            await ban_from_channel(client, not_abyding_users_from_channel)
             #
             await forbidden_group_ban(client, users_in_forbidden_groups)
         else:
             print("Purga cancelada\n")
             return
-
-
-
 
     except ValueError as e:
         raise e
@@ -184,13 +189,12 @@ async def purge_unwanted_users(client, channel, group, log_channel, forbidden_gr
 
 api_id, api_hash = env.API_ID, env.API_HASH
 
-# telethon_client = TelegramClient('session2', api_id, api_hash)
-# telethon_client.start(phone=env.PHONE, password=env.PASSWORD)
-
 telethon_client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
-print('logged in')
+print('Logged in')
+
 loop = asyncio.get_event_loop()
 
 loop.run_until_complete(purge_unwanted_users(telethon_client, CHANNEL, GROUP, LOG_CHANNEL, []))
-print("Done")
+# loop.run_until_complete(purge_unwanted_users(telethon_client,channels['Santa_Clara'], groups['Santa_Clara'],log_channels['Santa_Clara'],[]))
 
+print("Done")
